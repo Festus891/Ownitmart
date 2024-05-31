31c778b42d53994122a0b7c13269cc4f1deca272
Aderibigbe Festus Adewale <FESTUS891@YAHOO.COM>
2024-05-31 02:21:45 +0100

    initial commit

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB database connected with HOST:${con.connection.host}`
+      );
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+    });
+};
+module.exports = connectDatabase;
---
+++ frontend/src/actions/cartAction.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { ADD_TO_CART, REMOVE_ITEM_CART } from "../constants/cartConstant";
+
+// ADD ITEM TO CART
+export const addItemToCart = (id, quantity) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/v1/product/${id}`);
+
+  dispatch({
+    type: ADD_TO_CART,
+    payload: {
+      product: data.product._id,
+      name: data.product.name,
+      price: data.product.price,
+      image: data.product.images[0].url,
+      stock: data.product.stock,
+      quantity,
+    },
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+//REMOVE ITEM FROM CART
+export const removeItemFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: REMOVE_ITEM_CART,
+    payload: id,
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+//SAVE SHIPING INFO
+export const saveShippingInfo = (data) => async (dispatch) => {
+  dispatch({
+    type: REMOVE_ITEM_CART,
+    payload: data,
+  });
+
+  localStorage.setItem("shippingInfo", JSON.stringify(data));
+};
